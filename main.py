2c203b6e5b940122b760cf9458ee02d5c411550c
GuillaumeR <102474945+Kazulhu@users.noreply.github.com>
2022-05-27 12:26:40 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# Transverse Project: Game in python
+# Coded by:
+# Guillaume Rousselin
+# Fady Mechlaoui
+# Kevin Tran
+# Romain Ferigo
+# Quentin Rault
+
+# Main file of the project, it contains: the menu
+
 import pygame
 import sys
 from Button import Button
@@ -31,7 +41,7 @@ SCREEN.blit(play_button, (0, 0))
 def get_font(size):  # Returns Press-Start-2P in the desired size
     return pygame.font.Font("assets/font.ttf", size)
 
-
+# Page for the player 2 choice's
 def player2_perso():
     global player2_choice, player2_gravity
 
@@ -111,6 +121,7 @@ def player2_perso():
 
     return player2_choice
 
+# Function menu player 1
 def play():
     global player1_choice, player_gravity
 
@@ -194,13 +205,9 @@ def play():
 
     return player1_choice, player2_choice
 
+# Menu for the options
 def options():
     while True:
-        VOLUME = 100
-        volumeplus_button = Button(image=pygame.image.load("assets/volume_icon1.png"), pos=(640, 600),
-                                   text_input="", font=get_font(89), base_color="#d7fcd4", hovering_color="Green")
-        volumeminus_button = Button(image=pygame.image.load("assets/volume_icon2.png"), pos=(740, 600),
-                                    text_input="", font=get_font(89), base_color="#d7fcd4", hovering_color="Green")
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
         SCREEN.blit(BG, (0, 00))
@@ -208,37 +215,37 @@ def options():
 
         CONTROLS_TEXT1 = get_font(25).render("CONTROLS PLAYER 1", True, "Black")
         CONTROLS_TEXT2 = get_font(25).render("CONTROLS PLAYER 2", True, "Black")
-        CONTROLS_RECT1 = CONTROLS_TEXT1.get_rect(center=(300, 90))
-        CONTROLS_RECT2 = CONTROLS_TEXT2.get_rect(center=(900, 90))
+        CONTROLS_RECT1 = CONTROLS_TEXT1.get_rect(center=(360, 120))
+        CONTROLS_RECT2 = CONTROLS_TEXT2.get_rect(center=(960, 120))
 
         CONTROLS_PLAYER_3 = get_font(25).render("GO LEFT", True, "Black")
         CONTROLS_PLAYER_1 = get_font(25).render("JUMP", True, "Black")
         CONTROLS_PLAYER_2 = get_font(25).render("GO RIGHT", True, "Black")
-        CONTROLS_RAYER1 = CONTROLS_PLAYER_1.get_rect(center=(610, 190))
-        CONTROLS_RAYER2 = CONTROLS_PLAYER_2.get_rect(center=(610, 330))
-        CONTROLS_RAYER3 = CONTROLS_PLAYER_3.get_rect(center=(610, 480))
+        CONTROLS_RAYER1 = CONTROLS_PLAYER_1.get_rect(center=(650, 220))
+        CONTROLS_RAYER2 = CONTROLS_PLAYER_2.get_rect(center=(650, 360))
+        CONTROLS_RAYER3 = CONTROLS_PLAYER_3.get_rect(center=(650, 510))
 
-        UP_PLAYER1 = Button(image=pygame.image.load("assets/z.png"), pos=(290, 190),
+        UP_PLAYER1 = Button(image=pygame.image.load("assets/z.png"), pos=(350, 220),
                             text_input="", font=get_font(75), base_color="Black", hovering_color="Green")
         UP_PLAYER1.update(SCREEN)
-        RIGHT_PLAYER1 = Button(image=pygame.image.load("assets/d.png"), pos=(290, 330),
+        RIGHT_PLAYER1 = Button(image=pygame.image.load("assets/d.png"), pos=(350, 360),
                                text_input="", font=get_font(75), base_color="Black", hovering_color="Green")
         RIGHT_PLAYER1.update(SCREEN)
-        LEFT_PLAYER1 = Button(image=pygame.image.load("assets/q.png"), pos=(290, 480),
+        LEFT_PLAYER1 = Button(image=pygame.image.load("assets/q.png"), pos=(350, 510),
                               text_input="", font=get_font(75), base_color="Black", hovering_color="Green")
         LEFT_PLAYER1.update(SCREEN)
 
-        UP_PLAYER2 = Button(image=pygame.image.load("assets/up.png"), pos=(890, 190),
+        UP_PLAYER2 = Button(image=pygame.image.load("assets/up.png"), pos=(950, 220),
                             text_input="", font=get_font(75), base_color="Black", hovering_color="Green")
         UP_PLAYER2.update(SCREEN)
-        RIGHT_PLAYER2 = Button(image=pygame.image.load("assets/right.png"), pos=(890, 330),
+        RIGHT_PLAYER2 = Button(image=pygame.image.load("assets/right.png"), pos=(950, 360),
                                text_input="", font=get_font(75), base_color="Black", hovering_color="Green")
         RIGHT_PLAYER2.update(SCREEN)
-        LEFT_PLAYER2 = Button(image=pygame.image.load("assets/left.png"), pos=(890, 480),
+        LEFT_PLAYER2 = Button(image=pygame.image.load("assets/left.png"), pos=(950, 480),
                               text_input="", font=get_font(75), base_color="Black", hovering_color="Green")
         LEFT_PLAYER2.update(SCREEN)
 
-        OPTIONS_BACK = Button(image=pygame.image.load("DA/icons/shining/back_icon_s.png"), pos=(620, 610),
+        OPTIONS_BACK = Button(image=pygame.image.load("DA/icons/shining/back_icon_s.png"), pos=(650, 610),
                               text_input="", font=get_font(75), base_color="Black", hovering_color="Green")
 
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
@@ -259,7 +266,7 @@ def options():
                     main_menu()
         pygame.display.update()
 
-
+# Menu for the credits
 def credits():
     while True:
         CREDITS_MOUSE_POS = pygame.mouse.get_pos()
@@ -268,31 +275,31 @@ def credits():
         pygame.display.flip
 
         CREDITS_TEXT = get_font(70).render(" CREDITS ", True, "Green")
-        CREDITS_RECT = CREDITS_TEXT.get_rect(center=(640, 60))
+        CREDITS_RECT = CREDITS_TEXT.get_rect(center=(670, 90))
         CREDITS_TEXT1 = get_font(20).render("Coordinateur pôle codage", True, "Black")
-        CREDITS_RECT1 = CREDITS_TEXT1.get_rect(center=(640, 170))
+        CREDITS_RECT1 = CREDITS_TEXT1.get_rect(center=(670, 200))
         CREDITS_TEXT12 = get_font(20).render("Guillaume Rousselin", True, "Yellow")
-        CREDITS_RECT12 = CREDITS_TEXT12.get_rect(center=(640, 210))
+        CREDITS_RECT12 = CREDITS_TEXT12.get_rect(center=(670, 240))
 
         CREDITS_TEXT2 = get_font(20).render("Coordinateur du département graphique", True, "Black")
-        CREDITS_RECT2 = CREDITS_TEXT2.get_rect(center=(640, 250))
+        CREDITS_RECT2 = CREDITS_TEXT2.get_rect(center=(670, 280))
         CREDITS_TEXT22 = get_font(20).render("Quentin Rault", True, "Yellow")
-        CREDITS_RECT22 = CREDITS_TEXT22.get_rect(center=(640, 290))
+        CREDITS_RECT22 = CREDITS_TEXT22.get_rect(center=(670, 320))
 
         CREDITS_TEXT3 = get_font(20).render("Coordinateur pôle interface", True, "Black")
-        CREDITS_RECT3 = CREDITS_TEXT3.get_rect(center=(640, 330))
+        CREDITS_RECT3 = CREDITS_TEXT3.get_rect(center=(670, 360))
         CREDITS_TEXT32 = get_font(20).render("Kévin Tran", True, "Yellow")
-        CREDITS_RECT32 = CREDITS_TEXT32.get_rect(center=(640, 370))
+        CREDITS_RECT32 = CREDITS_TEXT32.get_rect(center=(670, 400))
 
         CREDITS_TEXT4 = get_font(20).render("Coordinateur de la partie physique", True, "Black")
-        CREDITS_RECT4 = CREDITS_TEXT4.get_rect(center=(640, 410))
+        CREDITS_RECT4 = CREDITS_TEXT4.get_rect(center=(670, 440))
         CREDITS_TEXT42 = get_font(20).render("Fady Mechlaoui", True, "Yellow")
-        CREDITS_RECT42 = CREDITS_TEXT42.get_rect(center=(640, 450))
+        CREDITS_RECT42 = CREDITS_TEXT42.get_rect(center=(670, 480))
 
         CREDITS_TEXT5 = get_font(20).render("Coordinateur pôle artistique", True, "Black")
-        CREDITS_RECT5 = CREDITS_TEXT5.get_rect(center=(640, 490))
+        CREDITS_RECT5 = CREDITS_TEXT5.get_rect(center=(670, 520))
         CREDITS_TEXT52 = get_font(20).render("Romain Ferigo", True, "Yellow")
-        CREDITS_RECT52 = CREDITS_TEXT52.get_rect(center=(640, 530))
+        CREDITS_RECT52 = CREDITS_TEXT52.get_rect(center=(670, 560))
 
         SCREEN.blit(CREDITS_TEXT, CREDITS_RECT)
         SCREEN.blit(CREDITS_TEXT1, CREDITS_RECT1)
@@ -306,7 +313,7 @@ def credits():
         SCREEN.blit(CREDITS_TEXT42, CREDITS_RECT42)
         SCREEN.blit(CREDITS_TEXT52, CREDITS_RECT52)
 
-        CREDITS_BACK = Button(image=pygame.image.load("DA/icons/shining/back_icon_s.png"), pos=(620, 630),
+        CREDITS_BACK = Button(image=pygame.image.load("DA/icons/shining/back_icon_s.png"), pos=(680, 660),
                               text_input="", font=get_font(75), base_color="Black", hovering_color="Green")
 
         CREDITS_BACK.changeColor(CREDITS_MOUSE_POS)
@@ -321,7 +328,7 @@ def credits():
                     main_menu()
         pygame.display.update()
 
-
+# Main menu
 def main_menu():
     SCREEN = pygame.display.set_mode((1360, 768))
     mixer.music.load('DA/sounds/musics/Head Soccer 95 bpm.mp3')
@@ -330,28 +337,24 @@ def main_menu():
     while True:
 
         SCREEN.blit(BG, (0, 0), )
-        SCREEN.blit(LOGO, (553, 30), )
+        SCREEN.blit(LOGO, (600, 30), )
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
         MENU_TEXT = get_font(70).render("FiRE   GOAL", True, "orange""")
-        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+        MENU_RECT = MENU_TEXT.get_rect(center=(690, 100))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("DA/icons/shining/start_icon_s.png"), pos=(640, 300),
+        PLAY_BUTTON = Button(image=pygame.image.load("DA/icons/shining/start_icon_s.png"), pos=(690, 330),
                              text_input="", font=get_font(89), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("DA/icons/shining/parameter_icon_s.png"), pos=(640, 540),
+        OPTIONS_BUTTON = Button(image=pygame.image.load("DA/icons/shining/parameter_icon_s.png"), pos=(690, 570),
                                 text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("DA/icons/shining/quit_icon_s.png"), pos=(100, 630),
+        QUIT_BUTTON = Button(image=pygame.image.load("DA/icons/shining/quit_icon_s.png"), pos=(100, 700),
                              text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        SOUND_BUTTON = Button(image=pygame.image.load("DA/icons/shining/music_off_s.png"), pos=(1145, 159),
+        SOUND_BUTTON = Button(image=pygame.image.load("DA/icons/shining/music_off_s.png"), pos=(1250, 159),
                               text_input="", font=get_font(100), base_color="#d7fcd4", hovering_color="White")
-        NOSOUND_BUTTON = Button(image=pygame.image.load("DA/icons/shining/music_on_s.png"), pos=(1145, 70),
+        NOSOUND_BUTTON = Button(image=pygame.image.load("DA/icons/shining/music_on_s.png"), pos=(1250, 70),
                                 text_input="", font=get_font(100), base_color="#d7fcd4", hovering_color="White")
-        CREDITS_BUTTON = Button(image=pygame.image.load("DA/icons/shining/info_icon_s.png"), pos=(1100, 630),
+        CREDITS_BUTTON = Button(image=pygame.image.load("DA/icons/shining/info_icon_s.png"), pos=(1250, 700),
                                 text_input="", font=get_font(100), base_color="#d7fcd4", hovering_color="White")
-        # SOUND_BUTTON1= pygame.image.load("assets/sound_logo.png")
-        # SOUND_BUTTON1 = pygame.transform.scale(SOUND_BUTTON1, (4,  10))
-        # SCREEN.blit(pygame.transform.scale(SOUND_BUTTON1, (0, 0), SOUND_BUTTON1))
-        # pygame.display.flip()
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
@@ -382,6 +385,5 @@ def main_menu():
 
         clock.tick(FPS)
         pygame.display.update()
-
 
 main_menu()
